e155e8342d861b6993a34c88c2b83de3f9e191ac
sttshka <sttshka.dev@gmail.com>
2026-03-02 19:06:33 +0300

    init

---
+++ src/components/icons/ChevronRightIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    class="h-4 w-4 shrink-0 transition-transform"
+    :class="expanded ? 'rotate-90' : ''"
+    aria-hidden="true"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M7.21 4.47a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06L8.27 14.03a.75.75 0 11-1.06-1.06L10.94 9.25 7.21 5.53a.75.75 0 010-1.06z"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  expanded: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
---
+++ src/components/icons/FileIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.5"
+    class="h-5 w-5 shrink-0"
+    aria-hidden="true"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M19.5 14.25v-2.625a3.375 3.375 0 00-.99-2.386l-4.125-4.125A3.375 3.375 0 0011.999 4.125H6.75A2.25 2.25 0 004.5 6.375v11.25a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V14.25z"
+    />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 4.5v4.875c0 .621.504 1.125 1.125 1.125h4.875" />
+  </svg>
+</template>
